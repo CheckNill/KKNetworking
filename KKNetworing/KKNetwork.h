acec0b76e61e449f49ed331dac475619dea7f47c
CheckNill <958865443@qq.com>
2018-08-28 17:51:42 +0800

    ..

--- KKNetworing/KKNetwork.h
+++ KKNetworing/KKNetwork.h
@@ -7,7 +7,9 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "AFNetworking.h"
+#import <SocketIO/SocketIO-Swift.h>
+#import "SVProgressHUD.h"
 
 @interface KKPlatformHttpSessionManager:AFHTTPSessionManager
 
